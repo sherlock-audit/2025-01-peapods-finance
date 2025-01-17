77b6347da36ca4359767fd08570a585e66b0d418
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2025-01-18 00:04:10 +0100

    Delete scope directories

--- contracts/contracts/interfaces/IDIAOracleV2.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-interface IDIAOracleV2 {
-    function getValue(string memory key) external view returns (uint128 price, uint128 timestamp);
-}
--- contracts/contracts/interfaces/IStakingConversionFactor.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-interface IStakingConversionFactor {
-    function getConversionFactor(address asset) external view returns (uint256 factor, uint256 denomenator);
-}
